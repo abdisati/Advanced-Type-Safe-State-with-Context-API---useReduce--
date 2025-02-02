7bb6b8469fb4a5c886e56bf149fda8068acaac83
Abdi Deresa <Abdishegute@gmail.com>
2025-02-01 16:21:05 -0800

    Import Timer type for improved type safety in Timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,5 @@
-import Container from './UI/Container.tsx';
+import Container from "./UI/Container.tsx";
+import { type Timer } from "../store/timers-context.tsx";
 
 export default function Timer() {
   return (
