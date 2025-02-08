6a9c518a555b6b72a5a4269155108829c1dd95e7
Abdi Deresa <Abdishegute@gmail.com>
2025-02-08 15:32:45 -0800

    Fix Timer component to properly clear interval on unmount and when time is up

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,15 +1,25 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer(props: TimerProps) {
   const { name, duration } = props;
+  const interval = useRef<number | null>(null);
   const [remainingTime, setRemaining] = useState(duration * 1000);
 
+  if (remainingTime <= 0 && interval.current !== null) {
+    clearInterval(interval.current);
+  }
+
   useEffect(() => {
-    setInterval(() => {
+    interval.current = setInterval(() => {
       setRemaining((prevTime) => prevTime - 50);
     }, 50);
+    return () => {
+      if (interval.current !== null) {
+        clearInterval(interval.current);
+      }
+    };
   }, []);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
